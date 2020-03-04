56eefc9a7a295dc44bb520938cd6b2e61dd1991f
John Wu <524306184@qq.com>
2020-03-04 11:36:15 +0800

    feat: 404 route

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/repos/:owner/:repo/contributors", contributorsBadgeHandler)
+	r.NoRoute(func(c *gin.Context) {
+		c.Redirect(302, "https://api.github.com/")
+	})
 
 	panic(r.Run())
 }
